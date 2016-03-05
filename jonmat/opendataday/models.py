e956dd0213ebf2f68f2254c52e9edc0f33664e76
Gyubin Son <geubin0414@gmail.com>
2016-03-05 15:39:28 +0900

    init django framework, models

---
+++ jonmat/opendataday/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+class CongressMan(models.Model):
+    def __init__(self, name, party, local):
+        self.name = name
+        self.party = party
+        self.local = local
+
+    name = models.CharField(max_length = 50)
+    party = models.CharField(max_length = 50)
+    local = models.CharField(max_length = 100)
+
+class Restaurant(models.Model):
+    def __init__(self, name, latitude, longitude, address):
+        self.name = name
+        self.latitude = latitude
+        self.longitude = longitude
+        self.address = address
+
+    def __init__(self, name, address):
+        self.name = name
+        self.address = address
+
+    def update(self, latitude, longitude):
+        self.latitude = latitude
+        self.longitude = longitude
+
+    name = models.CharField(max_length = 50)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    address = models.CharField(max_length = 250)
+
+class CongressRestaurantMap(models.Model):
+    congress = models.ForeignKey(CongressMan)
+    restaurant = models.ForeignKey(Restaurant)
