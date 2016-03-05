9a80c30f41dc80032121640ff2eea1994dc9d6dd
yangporo <529yhs@gmail.com>
2016-03-05 17:41:28 +0900

    add test data

--- jonmat/opendataday/views.py
+++ jonmat/opendataday/views.py
@@ -10,42 +10,43 @@ def get_data(request):
     except:
         pass
     str = """
+{
     data: [
-    {
-        restaurant_name: "식당이름",
-        latitude: 1.1,
-        longitude: 1.1,
-        category: "한식",
-        visitied: [{
-            party: "정당",
-            congressman_name: "의원이름",
-            local: "지역구",
-            price: "사용금액",
+        {
+            restaurant_name: "식당이름",
+            latitude: 1.1,
+            longitude: 1.1,
+            category: "한식",
+            visitied: [{
+                party: "정당",
+                congressman_name: "의원이름",
+                local: "지역구",
+                price: 111
+            }, {
+                party: "정당",
+                congressman_name: "의원이름",
+                local: "지역구",
+                price: 111
+            }]
         }, {
-            party: "정당",
-            congressman_name: "의원이름",
-            local: "지역구",
-            price: "사용금액",
-        }],
-    },
-    {
-        restaurant_name: "식당이름",
-        latitude: 1.1,
-        longitude: 1.1,
-        category: "한식",
-        visitied: [{
-            party: "정당",
-            congressman_name: "의원이름",
-            local: "지역구",
-            price: "사용금액",
-        }, {
-            party: "정당",
-            congressman_name: "의원이름",
-            local: "지역구",
-            price: "사용금액",
-        }]
-    }
-]
+            restaurant_name: "식당이름",
+            latitude: 1.1,
+            longitude: 1.1,
+            category: "한식",
+            visitied: [{
+                party: "정당",
+                congressman_name: "의원이름",
+                local: "지역구",
+                price: 111
+            }, {
+                party: "정당",
+                congressman_name: "의원이름",
+                local: "지역구",
+                price: 111
+            }]
+        }
+    ]
+}
 """
     return HttpResponse(str)
     # return JsonResponse(result)
