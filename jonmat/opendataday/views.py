dd9536faa4fe2d9df38a4b6be659216f09504995
yangporo <529yhs@gmail.com>
2016-03-05 16:59:02 +0900

    complete setting

--- jonmat/opendataday/views.py
+++ jonmat/opendataday/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-from jonmat.opendataday.models import CongressMan, Restaurant, CongressRestaurantMap
+from opendataday.models import CongressMan, Restaurant, CongressRestaurantMap
+from django.http import JsonResponse
 # Create your views here.
+
+def get_data(request):
+    result = {"data":[]}
+    try:
+        maps = CongressRestaurantMap.members.all()
+    except:
+        pass
+    return JsonResponse(result)
